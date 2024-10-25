d0e35172cdaed4bdac8da3fa74030c5e7dd6abc3
Dok6n <ehrbs2018@gmail.com>
2024-10-25 22:50:51 +0900

    chore(test): update vitest config with coverage options

--- vitest.config.ts
+++ vitest.config.ts
@@ -5,6 +5,9 @@ export default defineConfig({
   test: {
     coverage: {
       provider: 'v8',
+      reporter: ['text'],
+      reportsDirectory: './coverage',
+      exclude: ['node_modules/**', 'dist/**', 'examples/**', 'src/index.ts'],
     },
     globals: true,
     environment: 'jsdom',
